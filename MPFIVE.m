32276109a2b5f2e570c10d262281983ab74468b2
quetuagm <57588759+quetuagm@users.noreply.github.com>
2019-12-08 19:06:08 +0800

    Add files via upload

---
+++ MPFIVE.m
@@ -0,0 +1,20 @@
+formula=input('Enter Formula in terms of n: ','s');
+for n=0:1:199
+    ninline=inline(formula,'n');
+    xofn(n+1)=feval(ninline,n);
+end
+for n=0:1:199
+    if n==0
+        yofn(n+1)=(-1.5*xofn(n+1))+(2*xofn(n+2))-(0.5*xofn(n+3));
+    elseif (n>0) && (n<=198)
+        yofn(n+1)=(0.5*xofn(n+2))-(0.5*xofn(n));
+    else
+        yofn(n+1)=(1.5*xofn(n+1))-(2*xofn(n))+(0.5*xofn(n-1));
+    end
+end
+
+n=1:199;
+plot(n,xofn)
+hold on
+plot(n,yofn,'r')
+legend('x(n)','y(n)')
